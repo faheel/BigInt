e7c932e89f39e044b67ecdb640d92b45ac67c943
Faheel Ahmad <faheel@live.in>
2017-09-30 21:12:28 +0530

    Update test for assignment operators

--- test/operators/assignment.test.cpp
+++ test/operators/assignment.test.cpp
@@ -7,18 +7,48 @@
 #include "third_party/catch.hpp"
 
 
-TEST_CASE("Assignment operators", "[assignment][operators]") {
+TEST_CASE("Assigning an integer to BigInt", "[assignment][operators]") {
     BigInt num1 = 1234567890;
+    REQUIRE(num1 == 1234567890);
+
     BigInt num2;
-    num2 = num1;
-    REQUIRE(num2 == num1);
+    REQUIRE(num2 == 0);
+
+    num2 = 1234567890;
     REQUIRE(num2 == 1234567890);
+    REQUIRE(num2 == num1);
 
     num2 = 0;
     REQUIRE(num2 == 0);
+}
+
+TEST_CASE("Assigning a string to BigInt", "[assignment][operators]") {
+    /*
+        TODO: Make the following work:
+        BigInt num = "0";
+    */
+
+    BigInt num1;
+    num1 = "1234567890";
+    REQUIRE(num1 == "1234567890");
+
+    BigInt num2;
+    REQUIRE(num2 == "0");
 
     num2 = "1234567890";
     REQUIRE(num2 == "1234567890");
-    REQUIRE(num2 == 1234567890);
     REQUIRE(num2 == num1);
+
+    num2 = "0";
+    REQUIRE(num2 == "0");
+}
+
+TEST_CASE("Assigning a BigInt to BigInt", "[assignment][operators]") {
+    BigInt num1 = 1234567890;
+    BigInt num2 = num1;
+    REQUIRE(num2 == num1);
+    REQUIRE(num2 == 1234567890);
+
+    num2 = BigInt(0);
+    REQUIRE(num2 == 0);
 }
