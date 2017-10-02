8dc85d9fcce67f5bb5d88becb15b31a0fb3a1f8e
Faheel Ahmad <faheel@live.in>
2017-10-03 01:36:53 +0530

    Add += and -= arithmetic-assignment operators

---
+++ include/operators/arithmetic_assignment.hpp
@@ -0,0 +1,87 @@
+/*
+    ===========================================================================
+    Arithmetic-assignment operators
+    ===========================================================================
+*/
+
+#ifndef BIG_INT_ARITHMETIC_ASSIGNMENT_OPERATORS_HPP
+#define BIG_INT_ARITHMETIC_ASSIGNMENT_OPERATORS_HPP
+
+#include "BigInt.hpp"
+#include "constructors/constructors.hpp"
+#include "operators/assignment.hpp"
+#include "operators/binary_arithmetic.hpp"
+
+
+/*
+    BigInt += BigInt
+    ----------------
+*/
+
+BigInt& BigInt::operator+=(const BigInt& num) {
+    *this = *this + num;
+
+    return *this;
+}
+
+
+/*
+    BigInt -= BigInt
+    ----------------
+*/
+
+BigInt& BigInt::operator-=(const BigInt& num) {
+    *this = *this - num;
+
+    return *this;
+}
+
+
+/*
+    BigInt += Integer
+    -----------------
+*/
+
+BigInt& BigInt::operator+=(const long long& num) {
+    *this = *this + BigInt(num);
+
+    return *this;
+}
+
+
+/*
+    BigInt -= Integer
+    -----------------
+*/
+
+BigInt& BigInt::operator-=(const long long& num) {
+    *this = *this - BigInt(num);
+
+    return *this;
+}
+
+
+/*
+    BigInt += String
+    ----------------
+*/
+
+BigInt& BigInt::operator+=(const std::string& num) {
+    *this = *this + BigInt(num);
+
+    return *this;
+}
+
+
+/*
+    BigInt -= String
+    ----------------
+*/
+
+BigInt& BigInt::operator-=(const std::string& num) {
+    *this = *this - BigInt(num);
+
+    return *this;
+}
+
+#endif  // BIG_INT_ARITHMETIC_ASSIGNMENT_OPERATORS_HPP
