ba25af8c6f109eabc03c202e9b70c1d43f9e029e
Faheel Ahmad <faheel@live.in>
2017-12-04 00:25:07 +0530

    Add tests for *= operator

--- test/operators/arithmetic_assignment.test.cpp
+++ test/operators/arithmetic_assignment.test.cpp
@@ -8,7 +8,7 @@
 #include "third_party/catch.hpp"
 
 
-TEST_CASE("Add and assign", "[arithmetic-assignment][operators]") {
+TEST_CASE("Add and assign", "[arithmetic-assignment][operators][addition]") {
     BigInt num1, num2, num3;
     num1 = "85241964353410";
     num2 = "617705612502427";
@@ -26,7 +26,7 @@ TEST_CASE("Add and assign", "[arithmetic-assignment][operators]") {
     REQUIRE(num3 == "12347084806264822774");
 }
 
-TEST_CASE("Subtract and assign", "[arithmetic-assignment][operators]") {
+TEST_CASE("Subtract and assign", "[arithmetic-assignment][operators][subtraction]") {
     BigInt num1, num2, num3;
     num1 = "169523540030343";
     num2 = "849991085752380";
@@ -42,4 +42,26 @@ TEST_CASE("Subtract and assign", "[arithmetic-assignment][operators]") {
     REQUIRE(num3 == -1234567890);
     num3 -= "12345678909876543210";
     REQUIRE(num3 == "-12345678911111111100");
+}
+
+TEST_CASE("Multiply and assign", "[arithmetic-assignment][operators][multiplication]") {
+    BigInt num1, num2, num3;
+    num1 = "169523540030343";
+    num2 = "849991085752380";
+    num3 = 1;
+
+    num3 *= num1;
+    REQUIRE(num3 == "169523540030343");
+    num3 *= num2;
+    REQUIRE(num3 == "144093497850978300540184466340");
+    num3 *= num1 * num2;
+    REQUIRE(num3 == "20762936122929888116068405550279299956640101675030592995600");
+    num3 *= 0;
+    REQUIRE(num3 == 0);
+
+    num3 = 1;
+    num3 *= "-1234567890";
+    REQUIRE(num3 == "-1234567890");
+    num3 *= "-12345678909876543210";
+    REQUIRE(num3 == "15241578762383784111263526900");
 }
