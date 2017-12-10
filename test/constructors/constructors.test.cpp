7ed37ea2f0d8119842814861348de049b4583e72
Faheel Ahmad <faheel@live.in>
2017-12-10 07:17:45 +0530

    Add more tests for constructors

--- test/constructors/constructors.test.cpp
+++ test/constructors/constructors.test.cpp
@@ -1,15 +1,35 @@
+#include <climits>
+#include <random>
+
 #include "constructors/constructors.hpp"
 #include "operators/io_stream.hpp"
 #include "operators/relational.hpp"
 
 #include "third_party/catch.hpp"
 
-TEST_CASE("Constructors", "[constructors]") {
-    BigInt num1;
-    BigInt num2(0);
-    BigInt num3("0");
 
+TEST_CASE("Construct zero-valued BigInts", "[constructors]") {
+    BigInt num1;
     REQUIRE(num1 == 0);
-    REQUIRE(num2 == num1);
-    REQUIRE(num3 == num2);
+
+    BigInt num2(0);
+    REQUIRE(num2 == 0);
+
+    BigInt num3("0");
+    REQUIRE(num3 == 0);
+
+    BigInt num4(num3);
+    REQUIRE(num4 == 0);
+}
+
+TEST_CASE("Construct random BigInts", "[constructors]") {
+    std::random_device generator;
+    // uniform distribution of numbers from LONG_LONG_MIN to LONG_LONG_MAX:
+    std::uniform_int_distribution<long long>
+            distribution(LONG_LONG_MIN, LONG_LONG_MAX);
+    for (size_t i = 0; i < 30; i++) {
+        long long rand_num = distribution(generator);
+
+        REQUIRE(BigInt(rand_num) == rand_num);
+    }
 }
