5ce14b304b85761b1f53a14be5d6c4f6120552ff
Faheel Ahmad <faheel@live.in>
2017-12-13 03:38:11 +0530

    Add tests for division operator

--- test/operators/binary_arithmetic.test.cpp
+++ test/operators/binary_arithmetic.test.cpp
@@ -72,15 +72,14 @@ TEST_CASE("Chaining addition and subtraction",
     REQUIRE(num1 - num2 - num3 - num4 == -13298183);
 }
 
-TEST_CASE("Multiplication of BigInts",
-        "[binary-arithmetic][operators][multiplication]") {
-    BigInt num1;
-    BigInt num2;
+TEST_CASE("Multiplication of BigInts", "[binary-arithmetic][operators]"
+                                       "[multiplication]") {
+    BigInt num1, num2;
 
     std::random_device generator;
-    // uniform distribution of numbers from -3037000499 to 3037000499
-    std::uniform_int_distribution<long long> distribution(-FLOOR_SQRT_LONG_LONG_MAX,
-            FLOOR_SQRT_LONG_LONG_MAX);
+    // uniform distribution of numbers from -3037000499 to 3037000499:
+    std::uniform_int_distribution<long long>
+            distribution(-FLOOR_SQRT_LONG_LONG_MAX, FLOOR_SQRT_LONG_LONG_MAX);
     for (size_t i = 0; i < 30; i++) {
         long long rand_num1 = distribution(generator);
         long long rand_num2 = distribution(generator);
@@ -89,13 +88,35 @@ TEST_CASE("Multiplication of BigInts",
         num2 = rand_num2;
 
         long long product = rand_num1 * rand_num2;
-
-        REQUIRE(num1 * num2 == product);
     }
 }
 
-TEST_CASE("Multiplication of BigInts with integers and strings",
-        "[binary-arithmetic][operators][multiplication]") {
+TEST_CASE("Division of BigInts", "[binary-arithmetic][operators][division]") {
+    BigInt num1, num2;
+
+    std::random_device generator;
+    // uniform distribution of numbers from LONG_LONG_MIN to LONG_LONG_MAX:
+    std::uniform_int_distribution<long long>
+            distribution(LONG_LONG_MIN, LONG_LONG_MAX);
+    for (size_t i = 0; i < 30; i++) {
+        long long rand_num1 = distribution(generator);
+        long long rand_num2 = distribution(generator);
+
+        num1 = rand_num1;
+        num2 = rand_num2;
+        if (num2 == 0) {    // prevent division by zero
+            i--;
+            continue;
+        }
+
+        long long quotient = rand_num1 / rand_num2;
+
+        REQUIRE(num1 / num2 == quotient);
+    }
+}
+
+TEST_CASE("Multiplication and division of BigInts with integers and strings",
+        "[binary-arithmetic][operators][multiplication][division]") {
     BigInt num = 123;
     REQUIRE(num * 5 == 615);
     REQUIRE(num * -5 == -615);
@@ -105,6 +126,24 @@ TEST_CASE("Multiplication of BigInts with integers and strings",
     REQUIRE(num * "-5" == -615);
     REQUIRE(num * "456" == 56088);
     REQUIRE(num * "-456" == -56088);
+
+    REQUIRE(num / 5 == 24);
+    REQUIRE(num / -5 == -24);
+    REQUIRE(num / 456 == 0);
+    REQUIRE(num / -456 == 0);
+    REQUIRE(num / "5" == 24);
+    REQUIRE(num / "-5" == -24);
+    REQUIRE(num / "456" == 0);
+    REQUIRE(num / "-456" == 0);
+
+    // catch division by zero
+    try {
+        BigInt trouble = num / 0;
+    }
+    catch (std::logic_error e) {
+        CHECK(e.what() == std::string("Attempted division by zero"));
+    }
+
     /*
         TODO
         ----
@@ -112,6 +151,8 @@ TEST_CASE("Multiplication of BigInts with integers and strings",
         BigInt num = 123;
         REQUIRE(5 * num == 615);
         REQUIRE("-456" * num == -56088);
+        REQUIRE(5 / num == 0);
+        REQUIRE("-456" / num == -3);
     */
 }
 
@@ -150,4 +191,44 @@ TEST_CASE("Multiplication of really big numbers",
     num1 = big_pow10(19876);
     num2 = big_pow10(23450);
     REQUIRE(num1 * num2 == big_pow10(43326));
+}
+
+TEST_CASE("Division of really big numbers",
+        "[binary-arithmetic][operators][division][big]") {
+    BigInt num1, num2;
+    num1 = "12345678901234567890";
+    num2 = "1234567890";
+    REQUIRE(num1 / num2 == "10000000001");
+
+    num1 = "74795969103554554996215276693934490847811844274620";
+    num2 = "-5291857623565844660930182726104731881781491980";
+    REQUIRE(num1 / num2 == -14134);
+
+    num1 = "9582518950379800614306095260421646856475999938972041161410754824718"
+        "8530515989621711174263184271175858137696144827610415473041959151743248"
+        "42298961307365134004615";
+    num2 = "7183178499181562107254471437293951674461271038850005413302515662375"
+        "5697355361810132505715688137025455771038053164648324034388264089738986"
+        "976448753";
+    REQUIRE(num1 / num2 == "133402211172555");
+
+    num1 = "-920132912302829612061902393145744233945758297736906406054053889535"
+        "6968429805690068413076628064294801864511568551589205855316992620853084"
+        "3219577396774861536705894326785721164938367932913682323736670444";
+    num2 = "-126173815845557317670134103088446550114061753703252550511788939308"
+        "82979573617430830373800";
+    REQUIRE(num1 / num2 == "729258211092795635494143745485389681543083349270759"
+        "3708106029725015022394056069241322168656324094196515329458910");
+
+    num1 = big_pow10(2750);
+    num2 = big_pow10(1525);
+    REQUIRE(num1 / num2 == big_pow10(1225));
+
+    num1 = -big_pow10(5490);
+    num2 = big_pow10(3875);
+    REQUIRE(num1 / num2 == -big_pow10(1615));
+
+    num1 = big_pow10(23450);
+    num2 = big_pow10(19876);
+    REQUIRE(num1 / num2 == big_pow10(3574));
 }
