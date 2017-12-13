c98caebdcfd009f4d41e7c7b0ca83c872957a709
Faheel Ahmad <faheel@live.in>
2017-12-14 03:43:26 +0530

    Add tests for /= operator

--- test/operators/arithmetic_assignment.test.cpp
+++ test/operators/arithmetic_assignment.test.cpp
@@ -59,8 +59,30 @@ TEST_CASE("Multiply and assign", "[arithmetic-assignment][operators][multiplicat
     REQUIRE(num3 == 0);
 
     num3 = 1;
-    num3 *= "-1234567890";
-    REQUIRE(num3 == "-1234567890");
+    num3 *= -1234567890;
+    REQUIRE(num3 == -1234567890);
     num3 *= "-12345678909876543210";
     REQUIRE(num3 == "15241578762383784111263526900");
+}
+
+TEST_CASE("Divide and assign", "[arithmetic-assignment][operators][division]") {
+    BigInt num1, num2, num3;
+    num1 = "1695235400303";
+    num2 = "-14409349785097830054018446630";
+    num3 = "99609344092229394637577667754049630552571016118837570372569130682";
+
+    num3 /= num1;
+    REQUIRE(num3 == "58758414362055909808086078831411583704925762083687648");
+    num3 /= num2;
+    REQUIRE(num3 == "-4077797765921675738706461");
+    num3 /= num2 / num1;
+    REQUIRE(num3 == 479745944);
+    num3 /= num1;
+    REQUIRE(num3 == 0);
+
+    num3 = "15241578762383784111263526900";
+    num3 /= -1234567890;
+    REQUIRE(num3 == "-12345678909876543210");
+    num3 /= "-12345678909876543210";
+    REQUIRE(num3 == 1);
 }
