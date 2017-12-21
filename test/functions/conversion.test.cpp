794015e3d4c73afefeb15e49e89eb80b19201ab8
Faheel Ahmad <faheel@live.in>
2017-12-21 19:37:10 +0530

    Add tests for conversion functions

---
+++ test/functions/conversion.test.cpp
@@ -0,0 +1,57 @@
+#include <climits>
+#include <random>
+
+#include "constructors/constructors.hpp"
+#include "functions/conversion.hpp"
+#include "operators/io_stream.hpp"
+
+#include "third_party/catch.hpp"
+
+
+TEST_CASE("Convert BigInt to string", "[conversion][string]") {
+    std::random_device generator;
+    // uniform distribution of numbers from LONG_LONG_MIN to LONG_LONG_MAX:
+    std::uniform_int_distribution<long long>
+            distribution(LONG_LONG_MIN, LONG_LONG_MAX);
+    for (size_t i = 0; i < 20; i++) {
+        long long rand_num = distribution(generator);
+
+        REQUIRE(BigInt(rand_num).to_string() == std::to_string(rand_num));
+    }
+}
+
+TEST_CASE("Convert BigInt to int", "[conversion][int]") {
+    std::random_device generator;
+    // uniform distribution of numbers from INT_MIN to INT_MAX:
+    std::uniform_int_distribution<int>
+            distribution(INT_MIN, INT_MAX);
+    for (size_t i = 0; i < 20; i++) {
+        int rand_num = distribution(generator);
+
+        REQUIRE(BigInt(rand_num).to_int() == rand_num);
+    }
+}
+
+TEST_CASE("Convert BigInt to long int", "[conversion][long]") {
+    std::random_device generator;
+    // uniform distribution of numbers from LONG_MIN to LONG_MAX:
+    std::uniform_int_distribution<long>
+            distribution(LONG_MIN, LONG_MAX);
+    for (size_t i = 0; i < 20; i++) {
+        long rand_num = distribution(generator);
+
+        REQUIRE(BigInt(rand_num).to_long() == rand_num);
+    }
+}
+
+TEST_CASE("Convert BigInt to long long int", "[conversion][long long]") {
+    std::random_device generator;
+    // uniform distribution of numbers from LONG_LONG_MIN to LONG_LONG_MAX:
+    std::uniform_int_distribution<long long>
+            distribution(LONG_LONG_MIN, LONG_LONG_MAX);
+    for (size_t i = 0; i < 20; i++) {
+        long long rand_num = distribution(generator);
+
+        REQUIRE(BigInt(rand_num).to_long_long() == rand_num);
+    }
+}
