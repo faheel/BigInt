6da07d028e7f9b0aa321d68a20e43d1131e22bd2
Faheel Ahmad <faheel@live.in>
2017-12-30 18:20:37 +0530

    Fix exit code of test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+exit_code=0
+
 for test in $(find bin -name *.test | sort)
 do
     printf '%s:\n' $test
     $test
+    if [ $? != 0 ]      # test failed
+    then
+        exit_code=1
+    fi
 done
+
+exit $exit_code
