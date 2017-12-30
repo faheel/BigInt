f9b36fc2ded71448ddbca1224615b5da6ce24d1d
Faheel Ahmad <faheel@live.in>
2017-12-30 23:38:31 +0530

    Add script to generate integration test

---
+++ scripts/integrate_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+unit_tests="constructors/constructors.test.cpp \
+    functions/conversion.test.cpp \
+    functions/math.test.cpp \
+    operators/arithmetic_assignment.test.cpp \
+    operators/assignment.test.cpp \
+    operators/binary_arithmetic.test.cpp \
+    operators/increment_decrement.test.cpp \
+    operators/relational.test.cpp \
+    operators/unary_arithmetic.test.cpp"
+
+integration_test="test/integration.test.cpp"
+
+touch ${integration_test}
+for test in ${unit_tests}
+do
+    cat "test/${test}" >> ${integration_test}
+    printf "\n\n" >> ${integration_test}
+done
