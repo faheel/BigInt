72a4c9f95e10c9b1f0fbd7fdfd7a5347d8985dc4
Faheel Ahmad <faheel@live.in>
2018-01-01 03:50:46 +0530

    Check for exception when invalid argument is passed

--- test/constructors/constructors.test.cpp
+++ test/constructors/constructors.test.cpp
@@ -9,20 +9,21 @@
 
 
 TEST_CASE("Construct zero-valued BigInts", "[constructors]") {
-    BigInt num1;
+    BigInt num1;        // should be 0 by default
     REQUIRE(num1 == 0);
 
-    BigInt num2(0);
+    BigInt num2(0);     // 0 passed as an integer
     REQUIRE(num2 == 0);
 
-    BigInt num3("0");
+    BigInt num3("0");   // 0 passed as a string
     REQUIRE(num3 == 0);
 
-    BigInt num4(num3);
+    BigInt num4(num3);  // object with value 0 passed to copy constructor
     REQUIRE(num4 == 0);
 }
 
-TEST_CASE("Randomly construct BigInts from integers and strings", "[constructors]") {
+TEST_CASE("Randomly construct BigInts from integers and strings",
+          "[constructors][random][integer][string]") {
     std::random_device generator;
     // uniform distribution of numbers from LLONG_MIN to LLONG_MAX:
     std::uniform_int_distribution<long long> distribution((LLONG_MIN), (LLONG_MAX));
@@ -33,11 +34,18 @@ TEST_CASE("Randomly construct BigInts from integers and strings", "[constructors
         REQUIRE(BigInt(std::to_string(rand_num)) == rand_num);
     }
 
-    // catch invalid argument
+    // should throw exception when invalid arguments are passed:
     try {
-        BigInt trouble("123BigInt");
+        BigInt trouble("123BigInt");    // without sign
     }
-    catch (std::logic_error e) {
+    catch (std::logic_error &e) {
         CHECK(e.what() == std::string("Expected an integer, got \'123BigInt\'"));
+    }
+    try {
+        BigInt trouble("-4a5b6c");      // with sign
+                                        // looks like hex, but is still not allowed
+    }
+    catch (std::logic_error &e) {
+        CHECK(e.what() == std::string("Expected an integer, got \'-4a5b6c\'"));
     }
 }
