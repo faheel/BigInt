46868ed354e9b66570eedf9ba2f1db4639ea3fb1
Faheel Ahmad <faheel@live.in>
2017-12-31 19:52:20 +0530

    Improve performance of sqrt()
    by choosing an initial value close to the actual value

--- include/functions/math.hpp
+++ include/functions/math.hpp
@@ -9,6 +9,8 @@
 
 #include <string>
 
+#include "functions/conversion.hpp"
+
 
 /*
     abs
@@ -89,39 +91,38 @@ BigInt pow(const std::string& base, int exp) {
 /*
     sqrt
     ----
-    Returns the positive square root of a BigInt.
+    Returns the positive integer square root of a BigInt using Newton's method.
     NOTE: the input must be non-negative.
 */
 
-BigInt sqrt(const BigInt& num){
+BigInt sqrt(const BigInt& num) {
     if (num < 0)
         throw std::invalid_argument("Cannot compute square root of a negative integer");
+
+    // Optimisations for small inputs:
     if (num == 0)
         return 0;
     else if (num < 4)
         return 1;
+    else if (num < 9)
+        return 2;
+    else if (num < 16)
+        return 3;
 
-    BigInt sqrt_low = 1;
-    BigInt sqrt_high = 1;
-    BigInt better_estimate;
-    // estimate the integer square root using Newton's method:
-    while (!(sqrt_high * sqrt_high <= num and sqrt_low * sqrt_low >= num)) {
-        if (sqrt_high != 0)
-            better_estimate = (num / sqrt_high + sqrt_high) / 2;
-        else
-            better_estimate = 0;
-        sqrt_low = sqrt_high;
-        sqrt_high = better_estimate;
+    BigInt sqrt_prev = 0;
+    // The value for `sqrt_current` is chosen close to that of the actual
+    // square root.
+    // Since a number's square root has at least one less than half as many
+    // digits as the number,
+    //     sqrt_current = 10^(half_the_digits_in_num - 1)
+    BigInt sqrt_current = big_pow10(num.to_string().size() / 2 - 1);
+
+    while (abs(sqrt_current - sqrt_prev) > 1) {
+        sqrt_prev = sqrt_current;
+        sqrt_current = (num / sqrt_prev + sqrt_prev) / 2;
     }
 
-    if (sqrt_low == sqrt_high)      // estimates are the same
-        return sqrt_low;
-
-    // if estimates differ, return the one whose square is closest to num
-    if (abs(num - sqrt_low * sqrt_low) < abs(num - sqrt_high * sqrt_high))
-        return sqrt_low;
-    else
-        return sqrt_high;
+    return sqrt_current;
 }
 
 #endif  // BIG_INT_MATH_FUNCTIONS_HPP
