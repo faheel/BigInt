4932e30188d7ae12df1b34bda612b8d526cd648c
B1Z0N <kolausf@gmail.com>
2019-08-22 15:48:12 +0300

    Updated big_random tests to support new version of this function

--- test/functions/random.cpp
+++ test/functions/random.cpp
@@ -5,6 +5,10 @@
 #include "functions/random.hpp"
 #include "operators/io_stream.hpp"
 
+// for abs
+#include "functions/math.hpp"
+#include "operators/binary_arithmetic.hpp"
+
 #include "third_party/catch.hpp"
 
 
@@ -19,6 +23,6 @@ TEST_CASE("Generate random BigInts having a specified number of digits",
     size_t num_digits;
     for (int i = 0; i < 50; i++) {
         num_digits = rand_generator() % 10000;
-        REQUIRE(big_random(num_digits).to_string().size() == num_digits);
+        REQUIRE(abs(big_random(num_digits)).to_string().size() == num_digits);
     }
 }
