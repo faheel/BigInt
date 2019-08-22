b10f0d84681c38b1b8aee71ec79cbacaa7222e4d
B1Z0N <kolausf@gmail.com>
2019-08-22 16:07:57 +0300

    Returned previouse state of tests, all passed

--- test/functions/random.cpp
+++ test/functions/random.cpp
@@ -5,10 +5,6 @@
 #include "functions/random.hpp"
 #include "operators/io_stream.hpp"
 
-// for abs
-#include "functions/math.hpp"
-#include "operators/binary_arithmetic.hpp"
-
 #include "third_party/catch.hpp"
 
 
@@ -23,6 +19,6 @@ TEST_CASE("Generate random BigInts having a specified number of digits",
     size_t num_digits;
     for (int i = 0; i < 50; i++) {
         num_digits = rand_generator() % 10000;
-        REQUIRE(abs(big_random(num_digits)).to_string().size() == num_digits);
+        REQUIRE(big_random(num_digits).to_string().size() == num_digits);
     }
 }
