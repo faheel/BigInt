d99b98fc2bed75f82a3a0ce2e2b0111302a788ba
B1Z0N <kolausf@gmail.com>
2019-08-22 16:07:25 +0300

    Added right description for big_int function, and some distributions Returned positive number generation, that was removed due to misconception

--- include/functions/random.hpp
+++ include/functions/random.hpp
@@ -20,7 +20,7 @@ const size_t MAX_RANDOM_LENGTH = 1000;
 /*
     big_random (num_digits)
     -----------------------
-    Returns a random BigInt with a specific number of digits.
+    Returns a random positive BigInt with a specific number of digits.
 */
 
 BigInt big_random(size_t num_digits = 0) {
@@ -36,25 +36,24 @@ BigInt big_random(size_t num_digits = 0) {
     static std::seed_seq sseq{std::random_device{}()};
     // generator itself
     static std::mt19937 gen{sseq};                      
+    // different purpose distributions(cheap to create and use)
     static std::uniform_int_distribution<> digit_distr(0, 9);
-    static std::uniform_int_distribution<> first_digit_distr(-9, 9);
+    static std::uniform_int_distribution<> first_digit_distr(0, 9);
+    static std::uniform_int_distribution<> length_distribution(1, MAX_RANDOM_LENGTH);
     // number to generate
     static BigInt big_rand;
 
-    if (num_digits == 0) {
+    // first digit
+    big_rand.value = std::to_string(first_digit_distr(gen));
+
+    if (num_digits == 0)
         // the number of digits were not specified
         // use a random number for it:
-        num_digits = 1 + gen() % MAX_RANDOM_LENGTH;
-    }
-
-    // first digit and sign
-    auto first_digit = first_digit_distr(gen);
-    big_rand.value = std::to_string(abs(first_digit));
-    big_rand.sign = first_digit < 0 ? '-' : '+';
+        num_digits = length_distribution(gen);
 
     // insert other digits
     while (big_rand.value.size() < num_digits)
-    big_rand.value += std::to_string(digit_distr(gen));
+        big_rand.value += std::to_string(digit_distr(gen));
 
     return big_rand;
 }
